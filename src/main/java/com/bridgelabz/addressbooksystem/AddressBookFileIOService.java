83f5bbf82ea2f612b24567a19c71eb9d4b9dae51
smutallib97 <smutallib97@gmail.com>
2022-11-07 15:47:51 +0530

    Added: Read or Write the Address Book with person contact as CSV file

--- src/main/java/com/bridgelabz/addressbooksystem/AddressBookFileIOService.java
+++ src/main/java/com/bridgelabz/addressbooksystem/AddressBookFileIOService.java
@@ -1,2 +1,46 @@
-package com.bridgelabz.addressbooksystem;public class AddressBookFileIOService {
+package com.bridgelabz.addressbooksystem;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBookFileIOService {
+    private static final String HOME ="F:\\BridgeLabz\\LFP - 202\\JAVA Stream IO\\AddressBookIO\\src\\main\\resources\\";
+
+    public void writeData(List<Contact> contactList , String addressBookName) {
+        StringBuffer personBuffer = new StringBuffer();
+        contactList.forEach( person-> {
+            String contactDataString = person.toString().concat("\n");
+            personBuffer.append(contactDataString);
+
+        });
+        try {
+            Files.write(Paths.get(HOME+addressBookName + ".txt"),personBuffer.toString().getBytes());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Contact> readData (){
+        List<Contact> contactPersonList = new ArrayList<>();
+        try {
+            Files.lines(new File(HOME+"mumbai.txt").toPath())
+                    .map(line -> line.trim())
+                    .forEach(line->System.out.println(line));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return contactPersonList;
+    }
+    public void printData(){
+        try {
+            Files.lines(new File(HOME+"mumbai.txt").toPath())
+                    .forEach(System.out::println);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
